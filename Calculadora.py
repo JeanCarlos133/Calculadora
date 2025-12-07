161062b33a55dac0825de26da3880a60bd06ce72
JeanCarlos133 <jeancarlosdp133@gmail.com>
2025-12-07 17:41:06 -0400

    update variables names and sintaxies mistakes

--- Calculadora.py
+++ Calculadora.py
@@ -1,18 +1,18 @@
-def sum(n1, n2):
+def suma(n1, n2):
     resultado = n1 + n2
     return f"El resultado es: {resultado}"
 
-def rest(n1, n2):
+def resta(n1, n2):
     resultado = n1 - n2
     return f"El resultado es: {resultado}"
 
 
-def mult(n1, n2):
+def multiplicacion(n1, n2):
     resultado = n1 * n2
     return f"El resultado es: {resultado}"
 
 
-def div(n1, n2):
+def division(n1, n2):
     resultado = n1 / n2
     return f"El resultado es: {resultado}"
 
@@ -24,49 +24,56 @@ def porciento(porciento, n1):
     resultado = (porciento / 100) * n1
     return f"El {porciento}% de {n1} es: {resultado}"
 
+while True:
 
-print("opciones \n"
-"1. suma \n" 
-"2.resta \n" 
-"3.multiplicacion \n" 
-"4. divicion \n"
-"5. Elevacion \n" 
-"6. Pociento")
 
-opcion = input("Digite el numero de su opcion: ")
+    print("opciones \n"
+    "1. Suma \n" 
+    "2. Resta \n" 
+    "3. Multiplicacion \n" 
+    "4. Division \n"
+    "5. Elevacion \n" 
+    "6. Porciento \n"
+    "Q. Para salir")
 
-if opcion == "1":
-    n1 = int(input("Primer numero: "))
-    n2 = int(input("Segundo numero: "))
-    print(sum(n1, n2))
+    opcion = input("Digite el numero de su opcion: ").upper()
 
-elif opcion == "2":
-    n1 = int(input("Primer numero: "))
-    n2 = int(input("Segundo numero: "))
-    print(rest(n1, n2))
+    if opcion == "1":
+        n1 = int(input("Primer numero: "))
+        n2 = int(input("Segundo numero: "))
+        print(suma(n1, n2))
 
-elif opcion == "3":
-    n1 = int(input("Primer numero: "))
-    n2 = int(input("Segundo numero: "))
-    print(mult(n1, n2))
+    elif opcion == "2":
+        n1 = int(input("Primer numero: "))
+        n2 = int(input("Segundo numero: "))
+        print(resta(n1, n2))
 
-elif opcion == "4":
-    n1 = int(input("Primer numero: "))
-    n2 = int(input("Segundo numero: "))
-    print(div(n1, n2))
+    elif opcion == "3":
+        n1 = int(input("Primer numero: "))
+        n2 = int(input("Segundo numero: "))
+        print(multiplicacion(n1, n2))
 
-elif opcion == "5":
-    n1 = int(input("Primer numero: "))
-    n2 = int(input("Segundo numero: "))
-    print(elevacion(n1, n2))
+    elif opcion == "4":
+        n1 = int(input("Primer numero: "))
+        n2 = int(input("Segundo numero: "))
+        print(division(n1, n2))
 
-elif opcion == "6":
-    n_porciento = int(input("Introduzca el corciento: "))
-    n1 = int(input("Primer numero: "))
-    print(porciento(n_porciento, n1))
-    
+    elif opcion == "5":
+        n1 = int(input("Primer numero: "))
+        n2 = int(input("Segundo numero: "))
+        print(elevacion(n1, n2))
 
-else:
-    print("El digito introducido no es correcto, intentelo nuevamente")
+    elif opcion == "6":
+        n_porciento = int(input("Introduzca el porciento: "))
+        n1 = int(input("Primer numero: "))
+        print(porciento(n_porciento, n1))
 
+    elif opcion == "Q":
+        print("Saliendo del programa...")
+        break
+        
+    else:
+        print("El digito introducido no es correcto, intentelo nuevamente")
+
+print("El programa ha finalizado")
 
